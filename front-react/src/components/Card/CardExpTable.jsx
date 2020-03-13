2f7a8f1d9ac782441e85a8db95037cf7d8ae146c
tom <lim1017@hotmail.com>
2020-03-13 18:45:37 -0400

    removed fluff

---
+++ front-react/src/components/Card/CardExpTable.jsx
@@ -0,0 +1,55 @@
+/*!
+
+=========================================================
+* Light Bootstrap Dashboard React - v1.3.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
+* Copyright 2019 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+*/
+import React, { Component } from "react";
+
+export class CardExpTable extends Component {
+  render() {
+    return (
+      <div
+        className={"card" + (this.props.plain ? " card-plain" : "")}>
+        <div className={"header" + (this.props.hCenter ? " text-center" : "")}>
+          <h3 className="title">{this.props.category} {this.props.title}</h3>
+          <br />
+          <p className="category">{this.props.content2}</p>
+          
+        </div>
+        <div
+          className={
+            "content" +
+            (this.props.ctAllIcons ? " all-icons" : "") +
+            (this.props.ctTableFullWidth ? " table-full-width" : "") +
+            (this.props.ctTableResponsive ? " table-responsive" : "") +
+            (this.props.ctTableUpgrade ? " table-upgrade" : "")
+          }
+        >
+          {this.props.content}
+
+          <div className="footer">
+            {this.props.legend}
+            {this.props.stats != null ? <hr /> : ""}
+            <div className="stats">
+              <i className={this.props.statsIcon} /> {this.props.stats}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CardExpTable;
--- front-react/src/views/Goals.jsx
+++ front-react/src/views/Goals.jsx
@@ -1,40 +1,15 @@
-/*!
 
-=========================================================
-* Light Bootstrap Dashboard React - v1.3.0
-=========================================================
-
-* Product Page: https://www.creative-tim.com/product/light-bootstrap-dashboard-react
-* Copyright 2019 Creative Tim (https://www.creative-tim.com)
-* Licensed under MIT (https://github.com/creativetimofficial/light-bootstrap-dashboard-react/blob/master/LICENSE.md)
-
-* Coded by Creative Tim
-
-=========================================================
-
-* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-*/
 import React, { Component } from "react";
 import {
   Grid,
   Row,
-  Col,
-  FormGroup,
-  ControlLabel,
-  FormControl
+  Col
 } from "react-bootstrap";
 
 import { Card } from "components/Card/Card.jsx";
 import Goal from "components/Goal/index.jsx";
-import { FormInputs } from "components/FormInputs/FormInputs.jsx";
-import { UserCard } from "components/UserCard/UserCard.jsx";
-import Button from "components/CustomButton/CustomButton.jsx";
 import { StatsCard } from "components/StatsCard/StatsCard.jsx";
 import useAppData from "../hooks/useAppData";
-import { MDBDataTable } from 'mdbreact';
-
-import avatar from "assets/img/faces/face-3.jpg";
 
 function Goals(props) {
   const{
@@ -84,45 +59,6 @@ function Goals(props) {
                       setGoal={setGoal}
                     />
                   </div>
-
-                  // <div>
-                  //   <MDBDataTable
-                  //     scrollY
-                  //     maxHeight="300px"
-                  //     striped
-                  //     bordered
-                  //     small
-                  //     data={{
-                  //       columns: [
-                  //         {
-                  //           label: 'Name',
-                  //           field: 'name',
-                  //           sort: 'asc',
-                  //           width: 150
-                  //         },
-                  //         {
-                  //           label: 'Type',
-                  //           field: 'type',
-                  //           sort: 'asc',
-                  //           width: 150
-                  //         },
-                  //         {
-                  //           label: 'Amount',
-                  //           field: 'amount',
-                  //           sort: 'asc',
-                  //           width: 150
-                  //         },
-                  //         {
-                  //           label: 'Date',
-                  //           field: 'date',
-                  //           sort: 'asc',
-                  //           width: 150
-                  //         }
-                  //       ],
-                  //       rows: state.expenses
-                  //     }}
-                  //   />
-                  // </div>
                 }
               />
             </Col>
