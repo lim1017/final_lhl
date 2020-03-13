63636da3000dce246e9a4d5c5ee9c9db82f3ebb7
JehyunRa <jra4862@gmail.com>
2020-03-13 17:42:30 -0400

    commit before merge (#18)

--- front-react/src/components/Card/Card.jsx
+++ front-react/src/components/Card/Card.jsx
@@ -20,7 +20,8 @@ import React, { Component } from "react";
 export class Card extends Component {
   render() {
     return (
-      <div className={"card" + (this.props.plain ? " card-plain" : "")}>
+      <div
+        className={"card" + (this.props.plain ? " card-plain" : "")}>
         <div className={"header" + (this.props.hCenter ? " text-center" : "")}>
           <h4 className="title">{this.props.title}</h4>
           <p className="category">{this.props.category}</p>
--- front-react/src/variables/Variables.jsx
+++ front-react/src/variables/Variables.jsx
@@ -521,7 +521,7 @@ var dataSales = {
 };
 var optionsSales = {
   low: 0,
-  high: 800,
+  high: 12,
   showArea: false,
   height: "245px",
   axisX: {
@@ -574,9 +574,9 @@ var dataBar = {
   ]
 };
 var optionsBar = {
-  seriesBarDistance: 10,
+  seriesBarDistance: 5,
   axisX: {
-    showGrid: false
+    showGrid: true
   },
   height: "245px"
 };
