9ad3d04ef1357cdebe22e689d669be3d09137697
Tyler <yiqizhang@gmail.com>
2020-03-14 21:02:26 +0000

    null image rendering image

--- front-react/src/views/News.jsx
+++ front-react/src/views/News.jsx
@@ -3,12 +3,12 @@ import CardImg from "components/Card/CardImg.jsx";
 import { ProgressBar } from "react-bootstrap";
 import { articles } from "variables/EducationArticles.jsx";
 import MyVerticallyCenteredModal from "components/MyVerticallyCenteredModal/MyVerticallyCenteredModal.jsx";
+import noimage from "assets/img/sadpig.png";
 
 require("dotenv").config();
 
 const API_URL = "apidojo-yahoo-finance-v1.p.rapidapi.com";
 const API_KEY = process.env.REACT_APP_YAHOO_API_KEY;
-
 const unirest = require("unirest");
 
 class News extends Component {
@@ -57,7 +57,11 @@ class News extends Component {
                   key={uuid}
                   title={title}
                   link={link}
-                  image={main_image && main_image.original_url}
+                  image={
+                    main_image && main_image.original_url
+                      ? main_image.original_url
+                      : noimage
+                  }
                 />
               </div>
             );
