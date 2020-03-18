e1449b4620e0da4ada2547ee9d0bf0ab5374e9c7
tom <lim1017@hotmail.com>
2020-03-17 23:02:36 -0400

    forgot 1 thing

--- front-react/src/views/Expenses.jsx
+++ front-react/src/views/Expenses.jsx
@@ -248,47 +248,42 @@ function Dashboard(props) {
                          
                         if (data.type === 'slice') {
                       
-                          let pathLength = data.element._node.getTotalLength();
+                          var pathLength = data.element._node.getTotalLength();
+                          console.log(pathLength)
+    // Set a dasharray that matches the path length as prerequisite to animate dashoffset
+    data.element.attr({
+      'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
+    });
 
-                          console.log('pathLength: ', pathLength);
+    // Create animation definition while also assigning an ID to the animation for later sync usage
+    var animationDefinition = {
+      'stroke-dashoffset': {
+        id: 'anim' + data.index,
+        dur: 1000,
+        from: -pathLength + 'px',
+        to:  '0px',
+        // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
+        fill: 'freeze'
+      }
+    };
 
-                          data.element.attr({
-                            'stroke-dasharray': pathLength + 'px ' + pathLength + 'px'
-                          });
-                          
-                          // console.log('data: ', data);
-                          
-                          var animationDefinition = {
-                              "stroke-dashoffset": {
-                                id: "anim" + data.index,
-                                dur: 2000,
-                                from: -pathLength + "px",
-                                to: "0px",
-                                // We need to use `fill: 'freeze'` otherwise our animation will fall back to initial (not visible)
-                                fill: "freeze"
-                              }
-                          };
+    // If this was not the first slice, we need to time the animation so that it uses the end sync event of the previous animation
+    if(data.index !== 0) {
+      animationDefinition['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
+    }
 
-                          // console.log('animationDefinition', animationDefinition);
+    // We need to set an initial value before the animation starts as we are not in guided mode which would do that for us
+    data.element.attr({
+      'stroke-dashoffset': -pathLength + 'px'
+    });
 
-                          // if (data.index !== 0) {
-                          //   animationDefinition()['stroke-dashoffset'].begin = 'anim' + (data.index - 1) + '.end';
-                          // }
-
-                          console.log('data again: ', data);
-                        
-                          data.element.attr({
-                            'stroke-dashoffset': -pathLength + 'px'
-                          });
-
-                          console.log('data last time: ', data);
-
-                          console.log('animate function: ', data.element.animate);
-                        
-                          data.element.animate(animationDefinition);
+    // We can't use guided mode as the animations need to rely on setting begin manually
+    // See http://gionkunz.github.io/chartist-js/api-documentation.html#chartistsvg-function-animate
+    data.element.animate(animationDefinition, false);
+  }
                         }
                       }
-                    }} />
+                    } />
                 </div>
               }
               legend={
