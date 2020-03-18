06d9b9c96172e692547acd1b57fbe68ce68d0e3b
tom <lim1017@hotmail.com>
2020-03-18 16:22:38 -0400

    working on login

--- front-react/src/index.js
+++ front-react/src/index.js
@@ -19,6 +19,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
+// import TomNav from "./components/TomNav/TomNav.jsx";
+
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./assets/css/animate.min.css";
@@ -31,6 +33,7 @@ import AdminLayout from "layouts/Admin.jsx";
 
 ReactDOM.render(
   <BrowserRouter>
+
     <Switch>
       <Route path="/admin" render={props => <AdminLayout {...props} />} />
       <Redirect from="/" to="/admin/expenses" />
