477707736784f87f3fac57ec6696dd0f3263f464
Tyler <yiqizhang@gmail.com>
2020-03-20 00:31:32 +0000

    initial video uploaded

--- front-react/src/views/Youtube.jsx
+++ front-react/src/views/Youtube.jsx
@@ -2,24 +2,17 @@ import React, { useState, useEffect } from "react";
 import YouTube from "react-youtube";
 
 const Youtube = () => {
-  const [youtubeVideos, setYoutubeVideos] = useState();
-
-  // useEffect(() => {
-  //   youtubeVideos = "ivCY3Ec4iaU";
-  // }),
-  //   [];
+  const [youtubeVideo, setYoutubeVideo] = useState("PZBWI5vz2Hg");
 
   const opts = {
     height: "500",
     width: "100%",
     playerVars: {
-      // https://developers.google.com/youtube/player_parameters
       autoplay: 1
     }
   };
 
   const _onReady = event => {
-    // access to player in all event handlers via event.target
     event.target.pauseVideo();
   };
 
@@ -27,41 +20,41 @@ const Youtube = () => {
     <>
       <div className="youtube-video-container">
         <div className="youtube-video">
-          <YouTube videoId={youtubeVideos} opts={opts} onReady={_onReady} />
+          <YouTube videoId={youtubeVideo} opts={opts} onReady={_onReady} />
         </div>
         <div className="youtube-video-buttons-container">
           <button
-            onClick={() => setYoutubeVideos("PHe0bXAIuk0")}
+            onClick={() => setYoutubeVideo("PHe0bXAIuk0")}
             className="youtube-button"
           >
             How The Economy Works - Ray Dalio
           </button>
           <button
-            onClick={() => setYoutubeVideos("F3QpgXBtDeo")}
+            onClick={() => setYoutubeVideo("F3QpgXBtDeo")}
             className="youtube-button"
           >
             How The Stock Market Works - Kurzgesagt
           </button>
           <button
-            onClick={() => setYoutubeVideos("Dugn51K_6WA")}
+            onClick={() => setYoutubeVideo("Dugn51K_6WA")}
             className="youtube-button"
           >
             Money & Finance - Crash Course
           </button>
           <button
-            onClick={() => setYoutubeVideos("EfBSN0xTBo0")}
+            onClick={() => setYoutubeVideo("EfBSN0xTBo0")}
             className="youtube-button"
           >
             Financial Wisdom - Kevin Hart
           </button>
           <button
-            onClick={() => setYoutubeVideos("svbkVpeuwE4")}
+            onClick={() => setYoutubeVideo("svbkVpeuwE4")}
             className="youtube-button"
           >
             Compound Interest - Warren Buffet
           </button>
           <button
-            onClick={() => setYoutubeVideos("kpjZZBPQvDM")}
+            onClick={() => setYoutubeVideo("kpjZZBPQvDM")}
             className="youtube-button"
           >
             Financial Advice - Tony Robbins
