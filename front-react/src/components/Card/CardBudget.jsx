3563b99420b3396b6e93c14966d1d275900ae5ad
JehyunRa <jra4862@gmail.com>
2020-03-20 18:34:56 -0400

    James/budget34 (#53)
    * working on budget graph appearance
    
    * commit before rebase
    
    * commit before rebase
    
    * commit before push

---
+++ front-react/src/components/Card/CardBudget.jsx
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+
+export const Card = function(props) {
+
+    return (
+      <div
+        className={"cardBudget" +
+        (props.plain ? " cardBudget-plain" : "") +
+        (props.size ? ` cardBudgetSize${props.size}` : "")}
+      >
+        <div className={"header" + (props.hCenter ? " text-center" : "")}>
+          <h4 className="title">{props.title}</h4>
+          <p className="category">{props.category}</p>
+        </div>
+        <div
+          className={"content"}
+        >
+          {props.content}
+
+          <div className="footer">
+            {props.legend}
+            {props.stats != null ? <hr /> : ""}
+            <div className="stats">
+              <i className={props.statsIcon} /> {props.stats}
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+}
+
+export default Card;
