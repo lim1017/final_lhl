c8f616ae54b88bfa70d2e574d6ecc06b0ad9f7fa
tom <lim1017@hotmail.com>
2020-03-20 19:17:51 -0400

    styled sidebar box

---
+++ front-react/src/components/Card/CardSidebar.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from "react";
+
+export class CardSidebar extends Component {
+  render() {
+    return (
+      <div
+        className={"card" + (this.props.plain ? " card-plain" : "")}>
+        <div className={"header" + (this.props.hCenter ? " text-center" : "")}>
+          <h4 className="title">{this.props.title} {this.props.category}</h4>
+        </div>
+        <div
+          className={
+            "content" +
+            (this.props.ctAllIcons ? " all-icons" : "") +
+            (this.props.ctTableFullWidth ? " table-full-width" : "") +
+            (this.props.ctTableResponsive ? " table-responsive" : "") +
+            (this.props.ctTableUpgrade ? " table-upgrade" : "")
+          }
+        >
+          {this.props.content}
+
+          <div className="footer">
+            {this.props.legend}
+            {this.props.stats != null ? <hr /> : ""}
+            
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default CardSidebar;
