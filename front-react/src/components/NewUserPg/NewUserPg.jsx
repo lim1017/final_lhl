d34cd76d86e2d32043fab2768cfb0edecaf94e86
tom <lim1017@hotmail.com>
2020-03-22 02:23:10 -0400

    words to first user pg

--- front-react/src/components/NewUserPg/NewUserPg.jsx
+++ front-react/src/components/NewUserPg/NewUserPg.jsx
@@ -8,7 +8,8 @@ function NewUserPg(props) {
  <div>
     <h1>Hello BOB</h1>
     <h2>Welcome to piggy bank where we take your financial litter to the next level!</h2>
-    <h3>etc etc</h3>
+    <h3>etc etcetc etcetc etcetc etcetc etcetc etcetc etc</h3>
+    <h3>etc etcetc etcetc etcetc etc</h3>
     <h4> need to add button to take to actual dashboard </h4>
     </div>  );
 }
