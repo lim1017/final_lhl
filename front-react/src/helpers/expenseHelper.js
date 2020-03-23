f795b6f3b1e0569c374d92cd69f566ee3fbd479a
tom <lim1017@hotmail.com>
2020-03-23 00:21:11 -0400

    refactored expenses helper file

--- front-react/src/helpers/expenseHelper.js
+++ front-react/src/helpers/expenseHelper.js
@@ -1,30 +1,60 @@
-const createPie=function (expensesTotal) {
-   
-  const finalOP=[]
-  expensesTotal.forEach(element =>{
-    const slice={
-      name:element.type,
-      value:parseInt(element.sum)
-    }
-    finalOP.push(slice)
-  })
-  return finalOP
-}
+const createPie = function(expensesTotal) {
+  const finalOP = [];
+  expensesTotal.forEach(element => {
+    const slice = {
+      name: element.type,
+      value: parseInt(element.sum)
+    };
+    finalOP.push(slice);
+  });
+  return finalOP;
+};
 
+const returnMonthText = function(number) {
+  switch (number) {
+    case 1:
+      return "January";
+    case 2:
+      return "Febuary";
+    case 3:
+      return "March";
 
- const returnMonthText = function(number) {
-    switch (number) {
-      case 1:
-        return "January";
-      case 2:
-        return "Febuary";
-      case 3:
-        return "March";
-
-      default:
-      // code block
-    }
+    default:
+    // code block
   }
+};
 
-module.exports = { createPie, returnMonthText };
+const formatDataForExpenseTable = function(data) {
+  const finalOP = [];
 
+  data.forEach(ele => {
+    if (ele.amount !== 0) {
+      finalOP.push(ele);
+    }
+  });
+
+  return finalOP;
+};
+
+const formatDataForBarChart = function(data) {
+  const finalOP = [];
+  const avg = [315, 180, 533, 1700, 79, 172, 558, 300];
+  let i = 0;
+  data.forEach(ele => {
+    const bar = {
+      name: ele.type,
+      Personal: ele.sum,
+      Average: avg[i]
+    };
+    finalOP.push(bar);
+    i++;
+  });
+  return finalOP;
+};
+
+module.exports = {
+  formatDataForExpenseTable,
+  createPie,
+  returnMonthText,
+  formatDataForBarChart
+};
