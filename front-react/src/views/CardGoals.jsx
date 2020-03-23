d96b4ad0d40147a8f024995b15d1c69e97502ec1
JehyunRa <jra4862@gmail.com>
2020-03-23 14:51:13 -0400

    commit before master check

--- front-react/src/views/CardGoals.jsx
+++ front-react/src/views/CardGoals.jsx
@@ -3,6 +3,7 @@ import Grid from "@material-ui/core/Grid";
 import Goal from "components/Goal/index.jsx";
 
 function CardGoals(props) {
+
   return (
     <Grid item xs={12}>
       {
@@ -12,7 +13,7 @@ function CardGoals(props) {
             mode="new"
             key={props.state.goals.length + 1}
             id={props.state.goals.length + 1}
-            date={`2020-01-05T05:00:00.000Z`}
+            date={`1-${'JAN'}-${new Date().getFullYear() + 1}`}
             setGoal={props.setGoal}
           />
         </div>
