e488dcb1a61361a499feb1d6bf794b129d035092
Tyler <yiqizhang@gmail.com>
2020-03-26 00:52:20 +0000

    styling

--- front-react/src/views/CardGoals.jsx
+++ front-react/src/views/CardGoals.jsx
@@ -3,21 +3,16 @@ import Grid from "@material-ui/core/Grid";
 import Goal from "components/Goal/index.jsx";
 
 function CardGoals(props) {
-
   return (
     <Grid item xs={12}>
-      {
-        <div>
-          {props.GoalsInList}
-          <Goal
-            mode="new"
-            key={props.state.goals.length + 1}
-            id={0}
-            date={`1-${'JAN'}-${new Date().getFullYear() + 1}`}
-            setGoal={props.setGoal}
-          />
-        </div>
-      }
+      <Goal
+        mode="new"
+        key={props.state.goals.length + 1}
+        id={0}
+        date={`1-${"JAN"}-${new Date().getFullYear() + 1}`}
+        setGoal={props.setGoal}
+      />
+      <div className="goal-wrapper">{props.GoalsInList}</div>
     </Grid>
   );
 }
