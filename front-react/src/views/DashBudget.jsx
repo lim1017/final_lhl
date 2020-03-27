d910cdedc60c0c181403fecc1f693720b376c916
JehyunRa <jra4862@gmail.com>
2020-03-27 13:51:07 -0400

    James/button02 (#115)
    * css on budget
    
    * working on budget css
    
    * commit before push
    
    * adding responsiveness to budget
    
    * rebasing
    
    * rebasing
    
    * forgot 1 thing
    
    * expenses dash
    
    * removed junk]
    
    * worked on styling dashboard
    
    * words to first user pg
    
    * commit before fix push
    
    * a
    
    * pink buttons
    
    Co-authored-by: tom <lim1017@hotmail.com>

--- front-react/src/views/DashBudget.jsx
+++ front-react/src/views/DashBudget.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import MUButton from '@material-ui/core/Button';
 
 function DashBudget(props) {
+  const [button1, setButton1] = useState({color: 'linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)', x: 0});
+
+  const style = {
+    background: 'linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)',
+    borderRadius: 30,
+    border: 0,
+    color: 'white',
+    fontSize: 20,
+  };
+
   return (
     <div className="dashboard-container">
       <div className="dashboard-container-button">
         <Link to={`/admin/budget`}>
-          <button className="risk-assessment-start-button">Start Budget</button>
+          <MUButton
+            style={{
+              ...style,
+              background: button1.color,
+              height: 70 - button1.x,
+              width: 240 - button1.x,
+              margin: button1.x/2
+            }}
+            onMouseLeave={() => setButton1({
+              ...button1,
+              color: 'linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)',
+              x: 0
+            })}
+            onMouseOver={() => setButton1({
+              ...button1,
+              color: 'linear-gradient(45deg, #f06292 30%, #f8bbd0 90%)'
+            })}
+            onMouseUp={() => setButton1({
+              ...button1,
+              x: 0
+            })}
+            onMouseDown={() => setButton1({
+              ...button1,
+              x: 5
+            })}
+          >
+            Start Budget
+          </MUButton>
         </Link>
       </div>
     </div>
--- front-react/src/views/DashExpenses.jsx
+++ front-react/src/views/DashExpenses.jsx
@@ -1,16 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
-
-
-
-
-
+import MUButton from '@material-ui/core/Button';
 
 function DashExpenses(props) {
+  const [button1, setButton1] = useState({color: 'linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)', x: 0});
+
+  const style = {
+    background: 'linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)',
+    borderRadius: 30,
+    border: 0,
+    color: 'white',
+    fontSize: 20,
+  };
+
   return (
     <div className="dashboard-container">
       <Link to={`/admin/expenses`}>
-        <button className="risk-assessment-start-button">Start Expenses</button>
+        <MUButton
+          style={{
+            ...style,
+            background: button1.color,
+            height: 70 - button1.x,
+            width: 260 - button1.x,
+            margin: button1.x/2
+          }}
+          onMouseLeave={() => setButton1({
+            ...button1,
+            color: 'linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)',
+            x: 0
+          })}
+          onMouseOver={() => setButton1({
+            ...button1,
+            color: 'linear-gradient(45deg, #f06292 30%, #f8bbd0 90%)'
+          })}
+          onMouseUp={() => setButton1({
+            ...button1,
+            x: 0
+          })}
+          onMouseDown={() => setButton1({
+            ...button1,
+            x: 5
+          })}
+        >
+          Start Expenses
+        </MUButton>
       </Link>
     </div>
   );
