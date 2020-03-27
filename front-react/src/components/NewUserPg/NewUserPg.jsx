8255605e95f48b3df09e585091c80c8315533f1f
JehyunRa <jra4862@gmail.com>
2020-03-27 12:44:36 -0400

    a

--- front-react/src/components/NewUserPg/NewUserPg.jsx
+++ front-react/src/components/NewUserPg/NewUserPg.jsx
@@ -1,9 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
 import TextField from "@material-ui/core/Grid";
+import MUButton from '@material-ui/core/Button';
 
 function NewUserPg(props) {
   const username = localStorage.getItem("username");
+  const [button1, setButton1] = useState({color: 'linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)', x: 0});
+
+  const style = {
+    background: 'linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)',
+    borderRadius: 25,
+    border: 0,
+    color: 'white',
+    fontSize: 16,
+    height: 100,
+    padding: '0 50px 0 50px',
+    boxShadow: '0 3px 5px 2px #4a148c 30%',
+    marginLeft: 0,
+  };
 
   return (
     <>
@@ -39,6 +53,38 @@ function NewUserPg(props) {
       </div>
 
       <div className="new-user">
+
+      <MUButton
+          style={{
+            ...style,
+            background: button1.color,
+            width: 270 - button1.x,
+            marginLeft: button1.x,
+          }}
+          onMouseLeave={() => setButton1({
+            ...button1,
+            color: 'linear-gradient(45deg, #ec407a 30%, #f48fb1 90%)'
+          })}
+          onMouseOver={() => setButton1({
+            ...button1,
+            color: 'linear-gradient(45deg, #f06292 30%, #f8bbd0 90%)'
+          })}
+          onMouseUp={() => setButton1({
+            ...button1,
+            x: 0
+          })}
+          onMouseDown={() => setButton1({
+            ...button1,
+            x: 1
+          })}
+          onClick={() => {
+            console.log("clicked");
+            props.oldUser();
+          }}
+        >
+          Get Started Now
+        </MUButton>
+
         <button
           className="new-user-button"
           onClick={() => {
